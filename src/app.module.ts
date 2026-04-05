0da2b1acda676f8359ae52f11c8794ff02ff02aa
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2026-04-05 17:07:15 +0300

    feat: add empty 'comment' resource 	modified:   src/app.module.ts 	new file:   src/comment/comment.controller.ts 	new file:   src/comment/comment.module.ts 	new file:   src/comment/comment.service.ts 	new file:   src/comment/dto/create-comment.dto.ts 	new file:   src/comment/dto/update-comment.dto.ts 	new file:   src/comment/entities/comment.entity.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { ArticleModule } from './article/article.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, ArticleModule],
+  imports: [UserModule, CategoryModule, ArticleModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
