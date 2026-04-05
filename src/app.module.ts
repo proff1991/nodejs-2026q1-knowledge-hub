a88c8ca3ada575fee5cbb7ab24e204d9159d8945
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2026-04-05 14:14:30 +0300

    feat: add empty 'article' module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [UserModule, CategoryModule],
+  imports: [UserModule, CategoryModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
