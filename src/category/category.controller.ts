f7742120aa30dc995dbfbe3ddfb34680c39f8d07
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2026-04-05 14:13:37 +0300

    feat: add category module with logic

---
+++ src/category/category.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body
+  , Controller
+  , Delete
+  , Get
+  , HttpCode
+  , HttpStatus
+  , Param
+  , ParseUUIDPipe
+  , Post
+  , Put
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Controller('category')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) { }
+
+  @Post()
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoryService.create(createCategoryDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.categoryService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.categoryService.update(id, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.categoryService.remove(id);
+  }
+}
---
+++ src/category/category.service.ts
@@ -0,0 +1,66 @@
+import { randomUUID } from 'node:crypto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
+
+@Injectable()
+export class CategoryService {
+  private readonly categories: Map<string, Category> = new Map();
+
+  create(createCategoryDto: CreateCategoryDto): Category {
+    var category: Category = {
+      id: randomUUID(),
+      name: createCategoryDto.name,
+      description: createCategoryDto.description,
+    };
+
+    this.categories.set(category.id, category);
+
+    return category;
+  }
+
+  findAll(): Category[] {
+    return Array.from(this.categories.values());
+  }
+
+  findOne(id: string): Category {
+    var category = this.categories.get(id);
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    return category;
+  }
+
+  update(id: string, updateCategoryDto: UpdateCategoryDto): Category {
+    var category = this.categories.get(id);
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    if (typeof updateCategoryDto.name !== 'undefined') {
+      category.name = updateCategoryDto.name;
+    }
+
+    if (typeof updateCategoryDto.description !== 'undefined') {
+      category.description = updateCategoryDto.description;
+    }
+
+    this.categories.set(category.id, category);
+
+    return category;
+  }
+
+  remove(id: string): void {
+    var category = this.categories.get(id);
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    this.categories.delete(id);
+  }
+}
