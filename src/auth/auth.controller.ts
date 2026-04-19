6bde038fa7a24771e0a0f707b92056580dbf577c
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2026-04-19 20:49:41 +0300

    feat: implement auth.controller.ts, auth.service.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
 
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('signup')
+  signup(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
+}
